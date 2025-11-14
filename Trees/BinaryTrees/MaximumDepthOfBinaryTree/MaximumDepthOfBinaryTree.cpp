ffd771a9eb236252d7863ff7e254d9d06c9ec9fe
Stefan367 <stefan.ivanov673@gmail.com>
2025-11-14 11:14:58 +0200

    Add solution for 104. Maximum Depth of Binary Tree
    LeetCode problem

---
+++ Trees/BinaryTrees/MaximumDepthOfBinaryTree/MaximumDepthOfBinaryTree.cpp
@@ -0,0 +1,31 @@
+// LeetCode Problem 104: Maximum Depth of Binary Tree
+// Link: https://leetcode.com/problems/maximum-depth-of-binary-tree/
+// Description: Find the maximum depth (height) of a binary tree.
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+
+#include <algorithm>
+using namespace std;
+
+class Solution {
+public:
+    int maxDepth(TreeNode* root) {
+        if (!root)
+            return 0;
+
+        int leftMaxDepth = maxDepth(root->left);
+        int rightMaxDepth = maxDepth(root->right);
+
+        return max(leftMaxDepth, rightMaxDepth) + 1;
+    }
+};
